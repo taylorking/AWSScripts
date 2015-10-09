a07d71f2c5a6668f7fb989536429a801d65927eb
Taylor King <kingtb@appstate.edu>
2015-10-09 00:05:10 -0400

    Be more directory agnostic

--- lambdize.sh
+++ lambdize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 read -p "Function name? " name
 echo "Available Roles:"
 aws iam list-roles > .roles
@@ -10,15 +12,15 @@ if [ "$1" = "" ]; then
   read -p "Do you need a skeleton? " answer
   if [ $answer == 'y' ]; then
     read -p "What do you want me to call the file? " fileName
-    cat ~/.scripts/.skeleton.lambda > $fileName
+    cat ~/.scripts/.skeleton.lambda > $(pwd)/$fileName
   else
     read -p "I need a file to create the function, give me the name of a file: " fileName
   fi
 else 
-  set fileName=$1
+  fileName=$1
 fi
 echo "I will now zip the file ($fileName) and prepare for upload.. " 
-cp $fileName index.js  
-zip upload.zip index.js
-aws lambda create-function --function-name $name --runtime nodejs --role $role --handler index.handler --zip-file fileb://$(pwd)/upload.zip
-rm upload.zip index.js
+cp $(pwd)/$fileName $(pwd)/index.js  
+zip $(pwd)/upload.zip $(pwd)/index.js
+aws lambda create-function --function-name $name --runtime nodejs --role $role --handler index.handler --zip-file fileb://$(pwd)/upload.zip > .lambda.orig
+rm upload.zip
