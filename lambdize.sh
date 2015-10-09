c8b2f005f7cca9993c93350bff62c0e28c35a651
Taylor King <kingtb@appstate.edu>
2015-10-08 23:46:35 -0400

    this works

--- lambdize.sh
+++ lambdize.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 read -p "Function name? " name
 echo "Available Roles:"
-aws iam list-roles | grep RoleName | cut -d'"' -f 4
+aws iam list-roles > .roles
+cat .roles | grep "RoleName" | cut -d'"' -f4
 read -p "Role? " role
-read -p "Do you need a skeleton? " answer
-if [ $answer -eq 'y' ]; then
-  read -p "What do you want me to call the file?" fileName
-  cat ~/.scripts/.skeleton.lambda > $fileName
-  aws lambda create-function --function-name $name --runtime nodejs --role $role --handler index.handler --code=$fileName
-else; then
-  if [ -n $1 ]; then 
-    fileName = $1
-  else; then
-  read -p "I need a file to create the function, give me the name of a file" fileName
+
+role=$(cat .roles | grep $role | grep Arn |cut -d'"' -f4)
+if [ "$1" = "" ]; then
+  read -p "Do you need a skeleton? " answer
+  if [ $answer == 'y' ]; then
+    read -p "What do you want me to call the file? " fileName
+    cat ~/.scripts/.skeleton.lambda > $fileName
+  else
+    read -p "I need a file to create the function, give me the name of a file: " fileName
   fi
-  aws lambda create-function --function-name $name --runtime nodejs --role $role --handler index.handler --code=$fileName
+else 
+  set fileName=$1
 fi
+echo "I will now zip the file ($1) and prepare for upload.. " 
+  cp $fileName index.js  
+  zip upload.zip index.js
+  aws lambda create-function --function-name $name --runtime nodejs --role $role --handler index.handler --zip-file fileb://$(pwd)/upload.zip
+  rm upload.zip index.js
